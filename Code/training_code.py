9f7f443445c383a4e36525fa1b5d70b28dcab8bb
Ubuntu <ubuntu@ip-10-0-8-80.us-east-2.compute.internal>
2022-07-08 21:13:48 +0000

    Fixed code

--- Code/training_code.py
+++ Code/training_code.py
@@ -415,6 +415,7 @@ def main(n_epochs, model_name, model_save_flag, model_save_location, model_load_
 
 
 if __name__ == '__main__':
+    train_val_start_time = time.time()
     n_epochs = 10
     models = ['bert-base-uncased', 'roberta-base']
     
@@ -519,7 +520,11 @@ if __name__ == '__main__':
     all_best_ind_precision.to_csv('../validating_statistics/all_best_ind_precision.tsv', sep='\t')
     all_best_ind_recall.to_csv('../validating_statistics/all_best_ind_recall.tsv', sep='\t')
 
+    train_val_end_time = time.time()
+
+    total_time = (train_val_end_time - train_val_start_time) / 60
     print("Everything successfully completed")
+    print("Time to complete:", total_time)
 
     
 
